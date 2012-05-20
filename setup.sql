fe3cf7bca89133889f33028c14575e2acd23fe98
D Keith Casey Jr <keith@blueparabola.com>
2012-05-20 02:29:19 -0500

    table changes to add some audit type info;

--- setup.sql
+++ setup.sql
@@ -5,3 +5,6 @@ CREATE TABLE `subscribers` (
     `status` INT( 1 ) NOT NULL ,
     PRIMARY KEY ( `id` )
 ) ENGINE = MYISAM ;
+
+ALTER TABLE `subscribers` ADD `opt_in` DATETIME NOT NULL ,
+                          ADD `opt_out` DATETIME NOT NULL;
