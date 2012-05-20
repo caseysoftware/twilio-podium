9a83c83341f769d418bd7531f9885d40f964979e
D Keith Casey Jr <keith@blueparabola.com>
2012-05-20 01:38:51 -0500

    built most of the SMS flow;

---
+++ sms.php
@@ -0,0 +1,51 @@
+<?php
+$_POST = $_REQUEST;
+
+include 'creds.php';
+include 'functions.php';
+include 'Services/Twilio.php';
+
+$from = $_POST['From'];
+$from = preg_replace("/[^0-9]/", "", $from);
+
+$body = $_POST['Body'];
+$message = '';
+
+if (isset($whitelist[$from])) {
+    $body .= (strlen($body) <= 156) ? ' ~'.$whitelist[$from] : '';
+    $subscribers = get_subscribers();
+
+    $i = 0;
+    $client = new Services_Twilio($AccountSid, $AuthToken);
+    foreach($subscribers as $number) {
+
+//    $sms = $client->account->sms_messages->create(
+//            $twilioNumber,
+//$testNumber,
+//            $body
+//    );
+        $i++;
+    }
+    $message = "Your message was sent to $i subscribers";
+
+} else {
+    $body = strtolower($body);
+    $subscribed = is_subscribed($from);
+
+    $stop = strpos($body, 'stop');
+    if ($stop === false) {
+        if ($subscribed) {
+            $message = get_info_message();
+        } else {
+            subscribe($from);
+            $message = 'Welcome to "podium" the php|tek text messaging system powered by Twilio.';
+        }
+    } else {
+        unsubscribe($from);
+        $message = 'You have been opted out. To opt back in, send any message to this number';
+    }
+}
+
+//header('Content-type: text/xml');
+?>
+<Response><?php echo $message; ?></Response>
