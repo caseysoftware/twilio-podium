6a1ec58ea02df829249319b66dbf6f488f05a989
D Keith Casey Jr <keith@caseysoftware.com>
2012-08-25 01:08:37 -0500

    wiring commands into the mix

--- sms.php
+++ sms.php
@@ -3,44 +3,53 @@ include 'functions.php';
 
 $from = $_POST['From'];
 $from = preg_replace("/[^0-9]/", "", $from);
+$subscribed = is_subscribed($from);
 
 $body = $_POST['Body'];
-$message = '';
+$body = strtolower($body);
+$body = preg_replace("/[^A-Za-z0-9]/", "", $body );
+$keyword = substr($body, 0, 4);
 
-if (isset($whitelist[$from])) {
-    if ($body != '') {
-        $body .= (strlen($body) <= 156) ? ' ~'.$whitelist[$from] : '';
-        $subscribers = get_subscribers();
+switch ($keyword) {
+    case 'stop':
+        unsubscribe($from);
+        break;
+    case 'help':
+        sendMessage($from, $twilioNumber, $messages['sms-help']);
+        break;
+    case 'prev':
+        
+        break;
+    case 'next':
+        break;
+    case 'now':
+//TODO: schedule
+        break;
+    case 'join':
+        subscribe($from);
+        break;
+    default:
+        if (isset($whitelist[$from])) {
+            if ($body != '') {
+                $body .= (strlen($body) <= 156) ? ' ~'.$whitelist[$from] : '';
+                $subscribers = get_subscribers();
 
-        $i = 0;
-        foreach($subscribers as $subscriber) {
-            sendMessage($subscriber, $twilioNumber, $body);
-            $i++;
-        }
-        $body = "Your message was sent to $i subscribers";
-    } else {
-        $body = "You can't send an empty message to everyone. That's just rude!";
-    }
-    //NOTE: The variable is 'from' but this is who the message is sent *to*
-    sendMessage($from, $twilioNumber, $body);
-
-} else {
-    $body = strtolower($body);
-    $subscribed = is_subscribed($from);
-
-    $stop = strpos($body, 'stop');
-    if ($stop === false) {
-        if ($subscribed) {
-            $message = get_info_message($twilioNumber);
+                $i = 0;
+                foreach($subscribers as $subscriber) {
+                    sendMessage($subscriber, $twilioNumber, $body);
+                    $i++;
+                }
+                $body = "Your message was sent to $i subscribers";
+            } else {
+                $body = "You can't send an empty message to everyone. That's just rude!";
+            }
+            //NOTE: The variable is 'from' but this is who the message is sent *to*
+            sendMessage($from, $twilioNumber, $body);
         } else {
-            subscribe($from, $twilioNumber);
+            sendMessage($from, $twilioNumber, $messages['sms-help']);
         }
-    } else {
-        unsubscribe($from, $twilioNumber);
-    }
 }
 
-//TODO: convert to using the proper library response methods
 header('Content-type: text/xml');
 ?>
 <Response />
